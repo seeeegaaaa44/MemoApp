d25c5368f61e64206b93aa31f228a48081ecac11
sega <220478353+seeeegaaaa44@users.noreply.github.com>
2025-07-21 22:48:32 +0900

    Setup Firebase

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,8 +1,9 @@
 import { JSX } from 'react'
 import { View, Text, TextInput,TouchableOpacity, StyleSheet } from 'react-native'
+import { Link, router } from 'expo-router'
+import { useState } from 'react'
 
 import Button from '../../components/Button'
-import { Link, router } from 'expo-router'
 
 const handlePress = (): void => {
     // login
@@ -10,13 +11,31 @@ const handlePress = (): void => {
 }
 
 const LogIn = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 return(
 
     <View style={styles.container}>
         <View style={styles.inner}>
             <Text style={styles.title}>Log In</Text>
-            <TextInput style={styles.input} value='Email address' />
-            <TextInput style={styles.input} value='Password' />
+            <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={(text) => { setEmail(text) }}
+            autoCapitalize='none'
+            keyboardType='email-address'
+            placeholder='Email Address'
+            textContentType='emailAddress'
+             />
+            <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={(text) => { setPassword(text)}}
+            autoCapitalize='none'
+            secureTextEntry
+            placeholder='Password'
+            textContentType='password'
+            />
             <Button label='Submit' onPress={handlePress} />
             <View style={styles.footer}>
                 <Text style={styles.footerText}>Not registerd?</Text>
